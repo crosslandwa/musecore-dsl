b949fa4bbe3ee61e7ebe69692891680476fe96a7
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-01-31 10:21:24 +0000

    Drop use of EJS and replace with interpolated strings. Get things working for whole note length chords

--- example.js
+++ example.js
@@ -1,24 +1,25 @@
 const writeMscz = require('./lib/writeMscz.js')
-const { song, bar, chord, hold, rest } = require('./lib/dsl.js')
+const { song, measure, chord, hold, rest } = require('./lib/dsl')
 
 const x = 'x'
 const C = { offset: 0, name: 'C', notes: [x, 3, 2, 0, 1, 3] }
 const CAdd9 = { offset: 0, name: 'CAdd9', notes: [x, 3, 2, 0, 3, 3] }
 const D = { offset: 0, name: 'D', notes: [x, x, 0, 2, 3, 2] }
 const G = { offset: 0, name: 'G', notes: [3, 2, 0, 0, 3, 3] }
-const Em = { offset: 4, name: 'Em', notes: [x, 3, 5, 5, 6, 3] }
+const Em = { offset: 6, name: 'Em', notes: [0, 1, 3, 3, 2, 1] }
 
-writeMscz(
-  'A Test Song',
-  'Generic.ejs', // this might be fixed... I might supply different templates if the "song" has stuff that isn't generated through the DSL...
-  song([
-    bar(chord(CAdd9, 3.5), chord(G, 0.5)),
-    bar(hold(4)),
-    bar(chord(CAdd9, 3.5), chord(G, 0.5)),
-    bar(hold(4)),
-    bar(chord(Em, 3.5), chord(CAdd9, 0.5)),
-    bar(hold(3.5), chord(D, 0.5)),
-    bar(hold(4)),
-    bar(chord(D, 3), rest(1))
-  ])
+const songData = song('My song title', 'Ernie Bobbins',
+  measure(chord(CAdd9, 2), chord(D, 2)),
+  measure(chord(CAdd9, 1), chord(Em, 3)),
+  measure(chord(CAdd9, 1), chord(Em, 3)),
+  measure(chord(CAdd9, 1), chord(Em, 3)),
+  // measure(hold(4)),
+  // measure(chord(CAdd9, 3.5), chord(G, 0.5)),
+  // measure(hold(4)),
+  // measure(chord(Em, 3.5), chord(CAdd9, 0.5)),
+  // measure(hold(3.5), chord(D, 0.5)),
+  // measure(hold(4)),
+  // measure(chord(D, 3), rest(1))
 )
+console.log(songData)
+writeMscz('A Test Song', songData)
