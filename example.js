22951e035a6134103c9aafa0b9c3cc0a2ed88134
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-01-31 10:47:58 +0000

    Optional timesig / verse. First stab at handling eighth note chords

--- example.js
+++ example.js
@@ -8,11 +8,18 @@ const D = { offset: 0, name: 'D', notes: [x, x, 0, 2, 3, 2] }
 const G = { offset: 0, name: 'G', notes: [3, 2, 0, 0, 3, 3] }
 const Em = { offset: 6, name: 'Em', notes: [0, 1, 3, 3, 2, 1] }
 
+/*
+TODO
+[x] line breaks
+[x] hold and rest
+[x] non whole number of beat chords/holds/rests
+*/
+
 const songData = song('My song title', 'Ernie Bobbins',
-  measure(chord(CAdd9, 2), chord(D, 2)),
-  measure(chord(CAdd9, 1), chord(Em, 3)),
-  measure(chord(CAdd9, 1), chord(Em, 3)),
-  measure(chord(CAdd9, 1), chord(Em, 3)),
+  measure({text: 'verse 1', timeSig: '4/4'}, chord(CAdd9, 2.5), chord(D, 1.5)),
+  // measure(chord(CAdd9, 1), chord(Em, 3)),
+  // measure({text: 'verse2'}, chord(CAdd9, 1), chord(Em, 3)),
+  // measure(chord(CAdd9, 1), chord(Em, 3)),
   // measure(hold(4)),
   // measure(chord(CAdd9, 3.5), chord(G, 0.5)),
   // measure(hold(4)),
@@ -21,5 +28,4 @@ const songData = song('My song title', 'Ernie Bobbins',
   // measure(hold(4)),
   // measure(chord(D, 3), rest(1))
 )
-console.log(songData)
 writeMscz('A Test Song', songData)
