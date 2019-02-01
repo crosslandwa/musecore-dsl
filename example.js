3cda601f026e0c64bc50a61d8ced92d5c32c9d58
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-02-01 08:52:46 +0000

    Add repeats

--- example.js
+++ example.js
@@ -1,27 +1,31 @@
 const writeToMscx = require('./lib/writeToMscx.js')
-const { song, measure, chord, rest, lineBreak, text, timeSig } = require('./lib/dsl')
+const { chord, lineBreak, measure, repeatEnd, repeatStart, rest, song, text, timeSig } = require('./lib/dsl')
 
 const x = 'x'
-const C = { offset: 0, name: 'C', notes: [x, 3, 2, 0, 1, 3] }
-const CAdd9 = { offset: 0, name: 'CAdd9', notes: [x, 3, 2, 0, 3, 3] }
-const D = { offset: 0, name: 'D', notes: [x, x, 0, 2, 3, 2] }
-const G = { offset: 0, name: 'G', notes: [3, 2, 0, 0, 3, 3] }
-const Em = { offset: 6, name: 'Em', notes: [0, 1, 3, 3, 2, 1] }
+const C = { name: 'C', notes: [x, 3, 2, 0, 1, 3] }
+const Cmaj7 = { name: 'Cmaj7', notes: [x, 3, 2, 0, 0, 3] }
+const Cadd9 = { name: 'Cadd9', notes: [x, 3, 2, 0, 3, 3] }
+const D = { name: 'D', notes: [x, x, 0, 2, 3, 2] }
+const D6 = { name: 'Daug5', notes: [x, x, 0, 2, 0, 2] }
+const G = { name: 'G', notes: [3, 2, 0, 0, 3, 3] }
+const Em = { name: 'Em', notes: [0, 2, 2, 0, 0, 0] }
 
 /*
 TODO
 [x] rests
+[x] ring out
 */
+
 const songData = song('My song title', 'Ernie Bobbins',
-  measure('4/4')(text('verse 1'), timeSig(), chord(CAdd9, 1), chord(D, 4.5)),
-  measure('4/4')(chord(CAdd9, 1), chord(Em, 3)),
-  measure('3/4')(timeSig(), chord(CAdd9, 1), chord(Em, 3), lineBreak()),
+  measure('4/4')(text('verse 1'), timeSig(), chord(Cadd9, 1), chord(D, 4.5)),
+  measure('4/4')(chord(Cadd9, 1), chord(Em, 3)),
+  measure('3/4')(timeSig(), chord(Cadd9, 1), chord(Em, 3), lineBreak()),
   measure('3/4')(),
-  measure('4/4')(timeSig(), chord(CAdd9, 1), chord(Em, 3)),
+  measure('4/4')(timeSig(), chord(Cadd9, 1), chord(Em, 3)),
   measure('4/4')(),
-  measure('4/4')(chord(CAdd9, 1), chord(G, 4.5)),
+  measure('4/4')(chord(Cadd9, 1), chord(G, 4.5)),
   measure('4/4')(lineBreak()),
-  measure('4/4')(chord(Em, 1), chord(CAdd9, 4.5)),
+  measure('4/4')(chord(Em, 1), chord(Cadd9, 4.5), repeatStart(), repeatEnd()),
   measure('4/4')(chord(D, 4.5)),
   measure('4/4')(),
   measure('4/4')(chord(D, 3), rest(1))
